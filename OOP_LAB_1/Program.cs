832650f83419a879863a8a7aadd5097f7952f80a
dmyrych <dmyrych17@gmail.com>
2023-02-18 10:39:13 +0200

    task 2 is done

--- OOP_LAB_1/Program.cs
+++ OOP_LAB_1/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 
+
 namespace OOP_LAB_1
 {
     class MainClass
     {
+        delegate int randomInt();
+        delegate double averageInt(randomInt[] arr);
+        private static Random random = new Random();
+        //private static averageInt average(randomInt[] arr)
+        //{
+        //    int x = 0;
+        //    for(int i = 0; i < arr.Length; i++)
+        //    {
+        //        x += arr[i];
+        //    }
+        //    return ;
+        //}
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Answer to the task 1 is in the Calculation.cs
+            //Сalculations calculations = new Сalculations();
+            //calculations.DoThings();
+            //End of task 1
+            randomInt integer = () =>
+            {
+                return random.Next(1, 10);
+            };
+            randomInt[] randarr = new randomInt[10];
+            //averageInt getAverage = () => 
+            for(int i = 0; i < randarr.Length; i++)
+            {
+                randarr[i] = integer;
+            }
+            averageInt av = delegate (randomInt[] r)
+            {
+                double d = 0f;
+                for (int i = 0; i < r.Length; i++)
+                {
+                    d += r[i]();
+                }
+                return d/r.Length;
+            };
+            Console.WriteLine($"Average number is: {av(randarr)}");
         }
     }
 }
---
+++ OOP_LAB_1/Сalculations.cs
@@ -0,0 +1,81 @@
+﻿using System;
+namespace OOP_LAB_1
+{
+    public class Сalculations
+    {
+        delegate void Sum(int a, int b);
+        delegate void Min(int a, int b);
+        delegate void Div(int a, int b);
+        delegate void Mul(int a, int b);
+
+
+        public void DoThings()
+        {
+            Sum sum = (x, y) => Console.WriteLine($"{x} + {y} = {x + y}");
+            Min min = (x, y) => Console.WriteLine($"{x} - {y} = {x - y}");
+            Div div = (x, y) =>
+            {
+                if (y == 0)
+                {
+                    Console.WriteLine($"Failed to calculate {x} / {y}. You can't divide by zero!");
+                }
+                else
+                {
+                    Console.WriteLine($"{x} / {y} = {x / y}");
+                }
+
+            };
+            Mul mul = (x, y) => Console.WriteLine($"{x} * {y} = {x + y}");
+
+            string input;
+            int num1, num2;
+            Console.WriteLine("Hello, valued user! Would you like to sum(1), min(2), div(3), mult(4) two numbers? Just press a desired number on your keyboard");
+            int answ = Int32.Parse(Console.ReadLine());
+
+            switch (answ)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Good! Now enter two numbers, divided by a spacebar: ");
+                        input = Console.ReadLine();
+                        string[] splittedString = input.Split(' ');
+                        num1 = Int32.Parse(splittedString[0]);
+                        num2 = Int32.Parse(splittedString[1]);
+                        sum(num1, num2);
+                        break;
+                    }
+
+                case 2:
+                    {
+                        Console.WriteLine("Good! Now enter two numbers, divided by a spacebar: ");
+                        input = Console.ReadLine();
+                        string[] splittedString = input.Split(' ');
+                        num1 = Int32.Parse(splittedString[0]);
+                        num2 = Int32.Parse(splittedString[1]);
+                        min(num1, num2);
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Good! Now enter two numbers, divided by a spacebar: ");
+                        input = Console.ReadLine();
+                        string[] splittedString = input.Split(' ');
+                        num1 = Int32.Parse(splittedString[0]);
+                        num2 = Int32.Parse(splittedString[1]);
+                        div(num1, num2);
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Good! Now enter two numbers, divided by a spacebar: ");
+                        input = Console.ReadLine();
+                        string[] splittedString = input.Split(' ');
+                        num1 = Int32.Parse(splittedString[0]);
+                        num2 = Int32.Parse(splittedString[1]);
+                        mul(num1, num2);
+                        break;
+                    }
+            }
+        }
+    }
+}
